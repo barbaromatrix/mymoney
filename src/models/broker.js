4f2a12783fc9637b2e536677a70f7ad5d1c9da78
Matheus Galdino <matheus.galdino@concrete.com.br>
2019-01-01 13:39:26 -0200

    Initializing mymoney project

---
+++ src/models/broker.js
@@ -0,0 +1,15 @@
+
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Broker = sequelize.define(
+    'Broker',
+    {
+      name: { type: DataTypes.STRING, allowNull: false }
+    },
+    {}
+  );
+  Broker.associate = function (models) {
+    this.hasMany(models.Investment);
+  };
+  return Broker;
+}
